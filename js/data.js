09ada8b52c625097abcb1a05819fb0e7438766ca
Ykgarcia <79730258+YamnaKristell@users.noreply.github.com>
2022-08-09 23:48:11 -0600

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,39 @@
+public.loadPicture = function () {
+    var imageObj = new Image();
+    imageObj.src = '1.jpg';
+ 
+    imageObj.onload = function () {
+        context.drawImage( imageObj, 0, 0 );
+    }
+    var imageObj = new Image();
+    imageObj.src = '2.jpg';
+ 
+    imageObj.onload = function () {
+        context.drawImage( imageObj, 0, 0 );
+    }
+    var imageObj = new Image();
+    imageObj.src = '3.jpg';
+ 
+    imageObj.onload = function () {
+        context.drawImage( imageObj, 0, 0 );
+    }
+    var imageObj = new Image();
+    imageObj.src = '4.jpg';
+ 
+    imageObj.onload = function () {
+        context.drawImage( imageObj, 0, 0 );
+    }
+    var imageObj = new Image();
+    imageObj.src = '5.jpg';
+ 
+    imageObj.onload = function () {
+        context.drawImage( imageObj, 0, 0 );
+    }
+    var imageObj = new Image();
+    imageObj.src = '6.jpg';
+ 
+    imageObj.onload = function () {
+        context.drawImage( imageObj, 0, 0 );
+    }
+};
+
