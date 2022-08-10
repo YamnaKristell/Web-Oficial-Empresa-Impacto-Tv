33ddbd343a2858d73380a40765ed859b9fcce0e8
Ykgarcia <79730258+YamnaKristell@users.noreply.github.com>
2022-08-10 00:01:38 -0600

    Create info.js

---
+++ js/info.js
@@ -0,0 +1,39 @@
+export let user_ob = [
+    {
+        description:Paquetes Asombrosos,
+        price:'Desde Q150.00',
+
+    },
+  
+     {
+        description:Wifi de Alta Velocidad,
+        price:'Q175.00',
+
+    },
+   
+     {
+        description:Wifi Residencial,
+        price:'Q80.00',
+
+    },
+    
+     {
+        description:Tu canales Favoritos,
+        price:'Desde Q100.00',
+
+    },
+   
+     {
+        description:No te pierdas nuestras Actividades
+        
+
+    },
+    
+     {
+        description: Solo con un Click Vive la experiencia
+
+
+
+    },
+   
+   
